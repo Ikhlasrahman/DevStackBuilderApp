b932f5eed9d891ae8262357d513c0bfe331969ab
Ikhlas <ikhlas@Ikhlass-MacBook-Air.local>
2026-09-11 23:01:03 +0600

    Working on product card

---
+++ src/components/TechnologyCard.tsx
@@ -0,0 +1,15 @@
+import type {TechnologiesType} from '../types'
+
+type TechnologyCardProps = {
+    technology: TechnologiesType;
+};
+
+function TechnologyCard({technology}: TechnologyCardProps) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default TechnologyCard;
---
+++ src/components/YourStack.tsx
@@ -0,0 +1,9 @@
+function YourStack(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default YourStack;
